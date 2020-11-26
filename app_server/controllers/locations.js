cc20aa43b301717ca5d1561efaf8d640705ac6d8
DanSinCucci <72101909+DanSinCucci@users.noreply.github.com>
2020-11-26 17:59:12 -0500

    locations-list working (homepage) pg 109

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,17 +1,23 @@
 /* GET 'home' page */
-const homelist = (req, res) => {
-    res.render('locations-list', { title: 'Home' });
-    };
-    /* GET 'Location info' page */
-    const locationInfo = (req, res) => {
-    res.render('location-info', { title: 'Location info' });
-    };
-    /* GET 'Add review' page */
-    const addReview = (req, res) => {
-    res.render('location-review-form', { title: 'Add review' });
-    };
-    module.exports = {
-        homelist,
-        locationInfo,
-        addReview
-        };
+const homelist = function(req, res){
+  res.render('locations-list', {
+  title: 'Loc8r - find a place to work with wifi',
+  pageHeader: {
+  title: 'Loc8r',
+  strapline: 'Find places to work with wifi near you!'
+}
+});
+};
+/* GET 'Location info' page */
+const locationInfo = (req, res) => {
+  res.render('location-info', { title: 'Location info' });
+};
+/* GET 'Add review' page */
+const addReview = (req, res) => {
+  res.render('location-review-form', { title: 'Add review' });
+};
+module.exports = {
+  homelist,
+  locationInfo,
+  addReview
+};
