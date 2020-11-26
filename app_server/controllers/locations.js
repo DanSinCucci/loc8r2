8aec4ce5e5b943d3eb621eb52576cc930f6ce90a
DanSinCucci <72101909+DanSinCucci@users.noreply.github.com>
2020-11-26 14:47:03 -0500

    Map API working

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -4,7 +4,7 @@ const homelist = (req, res) => {
     };
     /* GET 'Location info' page */
     const locationInfo = (req, res) => {
-    res.render('index', { title: 'Location info' });
+    res.render('location-info', { title: 'Location info' });
     };
     /* GET 'Add review' page */
     const addReview = (req, res) => {
